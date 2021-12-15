75ca9082db6b38168e89d4d248fcf1bf37572ad7
Erik Morlin <emorlin@gmail.com>
2021-12-15 19:12:06 +0100

    Added sort to all fields.

--- gui/js/main.js
+++ gui/js/main.js
@@ -6,19 +6,23 @@ var bookClubApp = new Vue({
     spaceId: "cdz6eysa4lme",
     accessToken: "VoJLz9Wgu5ZR_2oKTnQmns2ApuKjUNpAIgNWJr0lMgM",
     booksRaw: "",
-    books: ""
+    books: "",
+    sortorder: "asc",
+    sortByField: "readDate",
   },
   created: function created() {},
   mounted: function mounted() {
     var _this = this;
-
     var client = contentful.createClient({
       space: this.spaceId,
-      accessToken: this.accessToken
+      accessToken: this.accessToken,
     });
-    client.getEntries().then(function (response) {
-      return _this.booksRaw = response.items;
-    }).catch(console.error);
+    client
+      .getEntries()
+      .then(function (response) {
+        return (_this.booksRaw = response.items);
+      })
+      .catch(console.error);
   },
   computed: {},
   watch: {
@@ -27,23 +31,41 @@ var bookClubApp = new Vue({
         var fields = _ref.fields;
         return fields;
       });
-      bookClubApp.sort("readDate");
-    }
+      bookClubApp.sort(this.sortByField);
+    },
   },
   methods: {
     sort: function sort(field) {
-      this.books.sort(this.sortBy(field));
+
+        if (field !== this.sortByField) {
+          this.sortorder="asc";
+          this.sortByField = field;
+        } else {
+          this.sortorder === "desc"
+            ? (this.sortorder = "asc")
+            : (this.sortorder = "desc");
+        }
+
+        this.books.sort(this.sortBy(field));
     },
-    sortBy: function sortBy(property, order) {
+    sortBy: function sortBy(property) {
       console.log("sorting");
-      if (typeof order === "undefined") order = "asc";
+      var that = this;
+
       return function (a, b) {
-        var varA = typeof a[property] === "string" ? a[property].toUpperCase() : a[property];
-        var varB = typeof b[property] === "string" ? b[property].toUpperCase() : b[property];
+        var varA =
+          typeof a[property] === "string"
+            ? a[property].toUpperCase()
+            : a[property];
+        var varB =
+          typeof b[property] === "string"
+            ? b[property].toUpperCase()
+            : b[property];
         var comparison = 0;
-        if (varA > varB) comparison = 1;else if (varA < varB) comparison = -1;
-        return order === "desc" ? comparison * -1 : comparison;
+        if (varA > varB) comparison = 1;
+        else if (varA < varB) comparison = -1;
+        return that.sortorder === "desc" ? comparison * -1 : comparison;
       };
-    }
-  }
+    },
+  },
 });
