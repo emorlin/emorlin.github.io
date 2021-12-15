150545748c4c3131e0f6047afb951ea7ee28c709
Erik Morlin <emorlin@gmail.com>
2021-12-15 21:02:58 +0100

    JS fixes

--- gui/js/main.js
+++ gui/js/main.js
@@ -8,21 +8,19 @@ var bookClubApp = new Vue({
     booksRaw: "",
     books: "",
     sortorder: "asc",
-    sortByField: "readDate",
+    sortByField: "readDate"
   },
   created: function created() {},
   mounted: function mounted() {
     var _this = this;
+
     var client = contentful.createClient({
       space: this.spaceId,
-      accessToken: this.accessToken,
+      accessToken: this.accessToken
     });
-    client
-      .getEntries()
-      .then(function (response) {
-        return (_this.booksRaw = response.items);
-      })
-      .catch(console.error);
+    client.getEntries().then(function (response) {
+      return _this.booksRaw = response.items;
+    }).catch(console.error);
   },
   computed: {},
   watch: {
@@ -30,18 +28,12 @@ var bookClubApp = new Vue({
       this.books = this.booksRaw.map(function (_ref) {
         return _ref.fields;
       });
-      this.books.forEach(
-        (book) =>{
-          book.avgGrade = (
-            (book.tomasGrade + book.mathiasGrade + book.eriksGrade) /
-            3
-          ).toFixed(2);
-
-          book.readDate = book.readDate.substring(0, book.readDate.length - 3);
-        }
-      );
+      this.books.forEach(function (book) {
+        book.avgGrade = ((book.tomasGrade + book.mathiasGrade + book.eriksGrade) / 3).toFixed(2);
+        book.readDate = book.readDate.substring(0, book.readDate.length - 3);
+      });
       bookClubApp.sort(this.sortByField);
-    },
+    }
   },
   methods: {
     sort: function sort(field) {
@@ -49,9 +41,7 @@ var bookClubApp = new Vue({
         this.sortorder = "asc";
         this.sortByField = field;
       } else {
-        this.sortorder === "desc"
-          ? (this.sortorder = "asc")
-          : (this.sortorder = "desc");
+        this.sortorder === "desc" ? this.sortorder = "asc" : this.sortorder = "desc";
       }
 
       this.books.sort(this.sortBy(field));
@@ -59,21 +49,13 @@ var bookClubApp = new Vue({
     sortBy: function sortBy(property) {
       console.log("sorting");
       var that = this;
-
       return function (a, b) {
-        var varA =
-          typeof a[property] === "string"
-            ? a[property].toUpperCase()
-            : a[property];
-        var varB =
-          typeof b[property] === "string"
-            ? b[property].toUpperCase()
-            : b[property];
+        var varA = typeof a[property] === "string" ? a[property].toUpperCase() : a[property];
+        var varB = typeof b[property] === "string" ? b[property].toUpperCase() : b[property];
         var comparison = 0;
-        if (varA > varB) comparison = 1;
-        else if (varA < varB) comparison = -1;
+        if (varA > varB) comparison = 1;else if (varA < varB) comparison = -1;
         return that.sortorder === "desc" ? comparison * -1 : comparison;
       };
-    },
-  },
+    }
+  }
 });
--- gui/source/js/main.js
+++ gui/source/js/main.js
@@ -1,54 +1,76 @@
+"use strict";
+
 var bookClubApp = new Vue({
   el: "#bookClubApp",
-
   data: {
     spaceId: "cdz6eysa4lme",
     accessToken: "VoJLz9Wgu5ZR_2oKTnQmns2ApuKjUNpAIgNWJr0lMgM",
     booksRaw: "",
     books: "",
+    sortorder: "asc",
+    sortByField: "readDate",
   },
-  created: function () {},
-  mounted: function () {
-    const client = contentful.createClient({
+  created: function created() {},
+  mounted: function mounted() {
+    var _this = this;
+    var client = contentful.createClient({
       space: this.spaceId,
       accessToken: this.accessToken,
     });
-
     client
       .getEntries()
-      .then((response) => (this.booksRaw = response.items))
+      .then(function (response) {
+        return (_this.booksRaw = response.items);
+      })
       .catch(console.error);
   },
   computed: {},
   watch: {
-    booksRaw: function () {
-      this.books = this.booksRaw.map(({ fields }) => fields);
-      bookClubApp.sort("readDate");
+    booksRaw: function booksRaw() {
+      this.books = this.booksRaw.map(function (_ref) {
+        return _ref.fields;
+      });
+      this.books.forEach((book) => {
+        book.avgGrade = (
+          (book.tomasGrade + book.mathiasGrade + book.eriksGrade) /
+          3
+        ).toFixed(2);
+
+        book.readDate = book.readDate.substring(0, book.readDate.length - 3);
+      });
+      bookClubApp.sort(this.sortByField);
     },
   },
   methods: {
-    sort: function (field) {
+    sort: function sort(field) {
+      if (field !== this.sortByField) {
+        this.sortorder = "asc";
+        this.sortByField = field;
+      } else {
+        this.sortorder === "desc"
+          ? (this.sortorder = "asc")
+          : (this.sortorder = "desc");
+      }
+
       this.books.sort(this.sortBy(field));
     },
-
-    sortBy: function (property, order) {
+    sortBy: function sortBy(property) {
       console.log("sorting");
-      if (typeof order === "undefined") order = "asc";
+      var that = this;
 
       return function (a, b) {
-        const varA =
+        var varA =
           typeof a[property] === "string"
             ? a[property].toUpperCase()
             : a[property];
-        const varB =
+        var varB =
           typeof b[property] === "string"
             ? b[property].toUpperCase()
             : b[property];
-
-        let comparison = 0;
+        var comparison = 0;
         if (varA > varB) comparison = 1;
         else if (varA < varB) comparison = -1;
-        return order === "desc" ? comparison * -1 : comparison;
+        return that.sortorder === "desc" ? comparison * -1 : comparison;
       };
     },
   },
